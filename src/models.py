79037577c788b0917c2fe4be78f9b8b6fbe18f58
Aaronsiox <jhoyamp@gmail.com>
2026-06-13 03:53:42 +0000

    My first version

--- src/models.py
+++ src/models.py
@@ -1,11 +1,14 @@
+from enum import Enum as pyEnum
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30))
+    lastname: Mapped[str] = mapped_column(String(30))
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -17,3 +20,35 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_from: Mapped[User] = relationship()
+    user_to: Mapped[User] = relationship()
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped[User] = relationship()
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(300))
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped[User] = relationship()
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped[Post] = relationship()
+
+class Type(pyEnum):
+    VIDEO = 1
+    PHOTO = 2
+    REEL = 3
+    STORIES = 4
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[Type] = mapped_column(Enum(Type))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped[Post] = relationship()
